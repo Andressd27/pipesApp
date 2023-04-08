cb8e03cb509607b6e6f24cb39312a077f747b230
Andres Duran <andurand27@gmail.com>
2023-04-07 21:32:09 -0500

    intro a i18nSelect y i18nPlural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
   styleUrls: ['./no-comunes.component.css']
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent{
 
-  constructor() { }
+  //i18nSelected
+  nombre: string = 'Andres';
+  genero: string = 'masculino';
 
-  ngOnInit(): void {
+  invitacionMapa = {
+    'masculino' : 'invitarlo',
+    'femenino'  : 'invitarla'
   }
 
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan', 'Andres', 'Juliana'];
+  clientesMapa = {
+    '=0' : 'no tenemos ningun cliente esperando.',
+    '=1' : 'tenemos un cliente esperando.',
+    'other' : 'tenemos # clientes esperando.'
+  }
 }
