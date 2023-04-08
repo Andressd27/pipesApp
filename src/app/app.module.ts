f2b7f05d7fd0340d03af83d1f112ee2812dc7057
Andres Duran <andurand27@gmail.com>
2023-04-07 19:01:27 -0500

    probando pipes basicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 //Modulo personalizado
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 import { AppRouterModule } from './app-router.module';
 
 @NgModule({
@@ -14,7 +15,8 @@ import { AppRouterModule } from './app-router.module';
   imports: [
     BrowserModule,
     AppRouterModule,
-    SharedModule
+    SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
